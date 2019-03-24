fa9443309bde9b64e5af1a5f142d61d7c8a8f32e
Alexandru Iuga <alexandru.iuga@iquestgroup.com>
2019-03-24 07:06:38 +0200

    updated license information.

--- sources/CollectionDiagrams/Properties/AssemblyInfo.cs
+++ sources/CollectionDiagrams/Properties/AssemblyInfo.cs
@@ -1,15 +1,31 @@
-﻿using System.Reflection;
+﻿// Diagrams
+// Copyright (C) 2015-2019 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dagrams")]
+[assembly: AssemblyTitle("Collection Diagrams")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dagrams")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Diagrams")]
+[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
